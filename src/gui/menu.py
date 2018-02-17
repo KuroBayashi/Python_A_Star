3bc5f131f0437ce33b4a4ff195b8d610bc776a62
KuroBayashi_Dev <kurobayashi.dev@gmail.com>
2018-02-17 02:28:49 +0100

    [Update] Ajout du choix de l'heuristique dans le menu.

--- src/gui/menu.py
+++ src/gui/menu.py
@@ -43,12 +43,34 @@ class Menu(Frame):
 
         return algo_label, algo_list
 
+    def build_heuristics_list(self):
+        # Label
+        heuristic_label = Label(self, text="Choix de l'heuristique : ")
+        heuristic_label.configure(font=font.Font(family=Menu.FONT_MAIN, size=12))
+        heuristic_label.configure(background=Menu.COLOR_BG, foreground=Menu.COLOR_FG)
+        heuristic_label.grid(sticky=W, padx=5)
+
+        # Radio buttons
+        heuristic_list = []
+        heuristic_list_def = [("Manhattan", "1"), ("Chebyshev", "2"), ("Euclidien", "3"), ("Octile", "4")]
+        x = IntVar(value=13)
+
+        for text, value in heuristic_list_def:
+            rdo = Radiobutton(self, text=text, variable=x, value=value, tristatevalue=0)
+            rdo.configure(font=font.Font(family=Menu.FONT_MAIN, size=10))
+            rdo.configure(background=Menu.COLOR_BG, foreground=Menu.COLOR_FG, selectcolor=Menu.COLOR_BG)
+            rdo.configure(activebackground=Menu.COLOR_BG, activeforeground=Menu.COLOR_FG)
+            rdo.grid(sticky=W, padx=20)
+            heuristic_list.append(rdo)
+
+        return heuristic_label, heuristic_list
+
     def build_options_list(self):
         # Options
-        options = Label(self, text="Options : ")
-        options.configure(font=font.Font(family=Menu.FONT_MAIN, size=12))
-        options.configure(background=Menu.COLOR_BG, foreground=Menu.COLOR_FG)
-        options.grid(sticky=W, padx=5)
+        opt_label = Label(self, text="Options : ")
+        opt_label.configure(font=font.Font(family=Menu.FONT_MAIN, size=12))
+        opt_label.configure(background=Menu.COLOR_BG, foreground=Menu.COLOR_FG)
+        opt_label.grid(sticky=W, padx=5)
 
         # Checkbox
         opt_list = []
@@ -63,7 +85,7 @@ class Menu(Frame):
             ckb.grid(sticky=W, padx=20)
             opt_list.append(ckb)
 
-        return opt_list
+        return opt_label, opt_list
 
     def build_start_button(self):
         # Button
@@ -88,6 +110,7 @@ class Menu(Frame):
 
         self.m_title = self.build_title()
         self.m_algorithms_label, self.m_algorithms_list = self.build_algorithms_list()
+        self.m_heuristics_label, self.m_heuristics_list = self.build_heuristics_list()
         self.m_options_list = self.build_options_list()
         self.m_start_button = self.build_start_button()
 
